61b255293f57fdd455dbc2ccfeb012ab4612cd55
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2025-02-17 01:13:50 -0600

    changed classes css in ProjectsDialog and ProjectsGridDialog

--- src/app/components/ProjectsGridDialog.tsx
+++ src/app/components/ProjectsGridDialog.tsx
@@ -8,12 +8,12 @@ const ProjectsGridDialog = () => {
   return (
     <ul
       role="list"
-      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-10/12 mx-auto gap-4 p-x-20"
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-10/12 mx-auto gap-10"
     >
       {projects.map((item) => (
         <li
           key={item.name}
-          className="justify-center col-span-1 flex flex-col h-full bg-gray-900 text-center border border-gold/50 rounded-lg shadow-md hover:shadow-lg transition-shadow w-full animateProjectCard"
+          className="justify-center col-span-1 flex flex-col h-full bg-gray-900 text-center border border-gold/50 rounded-lg shadow-md hover:shadow-lg transition-shadow w-full"
         >
           <Project
             key={item.name}
