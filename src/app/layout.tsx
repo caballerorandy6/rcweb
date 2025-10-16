f7b3564706f25a4dc1e5e9d1fd3bf33670011490
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2025-10-16 01:48:26 -0500

    created Linkedin partner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -152,22 +152,33 @@ export default function RootLayout({
 
         {/* LinkedIn Insight Tag */}
         {LINKEDIN_PARTNER_ID && (
-          <Script id="linkedin-insight" strategy="afterInteractive">
-            {`
-              _linkedin_partner_id = "${LINKEDIN_PARTNER_ID}";
-              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-              (function(l) {
-                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-                window.lintrk.q=[]}
-                var s = document.getElementsByTagName("script")[0];
-                var b = document.createElement("script");
-                b.type = "text/javascript";b.async = true;
-                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                s.parentNode.insertBefore(b, s);
-              })(window.lintrk);
-            `}
-          </Script>
+          <>
+            <Script id="linkedin-insight" strategy="afterInteractive">
+              {`
+                _linkedin_partner_id = "${LINKEDIN_PARTNER_ID}";
+                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                (function(l) {
+                  if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                  window.lintrk.q=[]}
+                  var s = document.getElementsByTagName("script")[0];
+                  var b = document.createElement("script");
+                  b.type = "text/javascript";b.async = true;
+                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                  s.parentNode.insertBefore(b, s);
+                })(window.lintrk);
+              `}
+            </Script>
+            <noscript>
+              <Image
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                alt=""
+                src={`https://px.ads.linkedin.com/collect/?pid=${LINKEDIN_PARTNER_ID}&fmt=gif`}
+              />
+            </noscript>
+          </>
         )}
       </head>
       <body
