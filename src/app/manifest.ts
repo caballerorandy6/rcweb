9f8e6aa789421830e6fcabfd98b2da8cf4c02429
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2025-12-10 01:28:22 -0600

    Remove favicon.ico from manifest to avoid duplication

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,11 +12,6 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#030712",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
-      },
-      {
         src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
